f9f973e9517d3e0672b07855c1699e5848fceaa3
HDRodrigues <helton.dias.rodrigues@gmail.com>
2022-05-03 13:56:33 -0300

    add fetch_move and fetch player

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,5 +1,7 @@
 defmodule ExMon do
   alias ExMon.{Game,Player}
+  alias ExMon.Game.Status
+  alias ExMon.Game.Actions
 
   @computer_name "Robotinik"
 
@@ -11,5 +13,11 @@ defmodule ExMon do
     @computer_name
     |> create_player(:punch, :kick, :heal)
     |> Game.start(player)
+
+    Status.print_round_message()
+  end
+
+  def make_move(move) do
+    Actions.fetch_move(move)
   end
 end
